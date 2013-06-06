db85d3e6bcec59fd4d5e7c37acd25e5cf5ed9535
Mike Vella <vellamike@gmail.com>
2013-06-07 00:47:07 +0100

    major refactoring which relies more heavily on elpa

--- init.el
+++ init.el
@@ -1,46 +1,73 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(require 'cl)
 
 (require 'package)
+(setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("technomancy" . "http://repo.technomancy.us/emacs/")))
+
+(setq ash-packages
+      '(ace-jump-mode
+        anaphora
+        autopair
+        bang
+        color-theme-solarized
+        cppcheck
+        dart-mode
+        dynamic-fonts
+        expand-region
+        fill-column-indicator
+        flex-autopair
+        flymake-cursor
+        flyspell-lazy
+        font-utils
+        idle-highlight-mode
+        ido-ubiquitous
+        jabber
+        js2-mode
+        key-chord
+        list-utils
+        magit
+        oauth2
+        paredit
+        rainbow-delimiters
+        rainbow-mode
+        smex
+        starter-kit
+        undo-tree
+        yasnippet
+        pymacs
+        auto-complete
+        ))
+
 (package-initialize)
+;;; install missing packages
+(let ((not-installed (remove-if 'package-installed-p ash-packages)))
+  (if not-installed
+      (if (y-or-n-p (format "there are %d packages to be installed. install them? "
+                            (length not-installed)))
+          (progn (package-refresh-contents)
+                 (dolist (package not-installed)
+                   (package-install package))))))
 
- ;; ido mode
-(ido-mode t)
-
- ;;column numbers:
-(column-number-mode t)
-
-;;undo tree mode
-(add-hook 'after-init-hook 'global-undo-tree-mode)
+(add-hook 'after-init-hook 'global-undo-tree-mode) ;;undo-tree mode
 
 ;;having some trouble with fonts in terminal, choose a monospaced
 (if (equal (display-graphic-p) 'false) (set-default-font "Inconsolata-12")
 )
 
-(setq initial-scratch-message "")
+(setq initial-scratch-message "") ;;get rid of annoying scratch message
+
+;;auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+(global-auto-complete-mode t)
 
 ;;python
 (add-to-list 'load-path "~/.emacs.d/pymacs") ;;mv experiment
-(require 'pymacs)
 (pymacs-load "ropemacs" "rope-")
 (setq ropemacs-enable-autoimport t)
-(add-to-list 'load-path "~/.emacs.d/auto-complete-1.3.1")
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete-1.3.1/dict")
-(ac-config-default)
-;;get python indentation right: - MV temporarily canceled this to see what effect would be
-;;(add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-m" 'ne;;wline-and-indent)))
-
 (setq
  python-shell-interpreter "ipython"
  python-shell-interpreter-args ""
@@ -52,18 +79,16 @@
    "';'.join(module_completion('''%s'''))\n"
  python-shell-completion-string-code
    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
-
-
+;;get python indentation right: - MV temporarily canceled this to see what effect would be
+;;(add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-m" 'ne;;wline-and-indent)))
 
 ;; enable yasnippet
-(add-to-list 'load-path
-              "~/.emacs.d/yasnippet") (require 'yasnippet) 
 (yas-global-mode 1)
 
 ;; Auto-Save on ^Z
 (add-hook 'suspend-hook 'do-auto-save)
 
-
+;;fullscreen
 (defun toggle-fullscreen ()
   (interactive)
   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
@@ -73,48 +98,6 @@
 )
 (toggle-fullscreen)
 
-;(when (display-graphic-p)
-;  (defun toggle-fullscreen ()
-;    (interactive)
-;    (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-;			   '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
-;    (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-;			   '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
-;    )
-;(toggle-fullscreen)
-;)
-
-;package requirement
-(require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
-
-;select color theme
+;;select color theme
 (color-theme-initialize)
 (color-theme-solarized-dark)
-
-
-;Playing around with gnus:
-
-(setq gnus-select-method '(nnimap "gmail"
-				  (nnimap-address "imap.gmail.com")
-				  (nnimap-server-port 993)
-				  (nnimap-stream ssl)))
-
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-;;Some stuff I'm not currently using but would like to keep here for reference:
-
-;(require 'color-theme)
-;;; colour themes:
-;(add-to-list 'load-path "~/.emacs.d/emacs-color-theme-solarized")
-;(if
-;    (equal 0 (string-match "^24" emacs-version))
-;    ;; it's emacs24, so use built-in theme
-;    (require 'solarized-dark-theme)
-;  ;; it's NOT emacs24, so use color-theme
-;  (progn
-;    (require 'color-theme)
-;    (color-theme-initialize)
-;    (require 'color-theme-solarized)
-;    (color-theme-solarized-light)))
