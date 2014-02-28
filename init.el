0de816c7e73757580da7f582d8125acade229c4a
Mike Vella <vellamike@gmail.com>
2014-02-28 21:04:24 +0000

    Custom set variables, default smaller

--- init.el
+++ init.el
@@ -56,7 +56,7 @@
 (add-hook 'after-init-hook 'global-undo-tree-mode) 
 
 ;;monospaced font - Inconsolata should be installed if not available
-(set-default-font "Inconsolata-13")
+(set-default-font "Inconsolata-12")
 
 ;;get rid of annoying scratch message
 (setq initial-scratch-message "")
@@ -92,3 +92,15 @@
 (add-hook 'python-mode-hook
           (lambda ()
             (define-key python-mode-map "\r" 'newline-and-indent)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
