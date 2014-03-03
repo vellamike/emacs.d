7c527ac30b4ba16b80aa03786efd0f459df9f234
Mike Vella <vellamike@gmail.com>
2014-03-03 00:04:53 +0000

    Back to solarized-dark theme.

--- init.el
+++ init.el
@@ -36,6 +36,8 @@
         yasnippet
         pymacs
         auto-complete
+        solarized-theme
+        zenburn-theme
         ))
 
 (package-initialize)
@@ -83,7 +85,7 @@
 ;;;;(if (eq window-system 'X) (toggle-fullscreen))
 
 ;;select color theme
-(load-theme 'wombat)
+(load-theme 'solarized-dark t)
 
 ;;python-mode indentation
 ;;this is a matter of taste, when I hit ENTER and I'm in a method I
@@ -92,15 +94,18 @@
 (add-hook 'python-mode-hook
           (lambda ()
             (define-key python-mode-map "\r" 'newline-and-indent)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "0f0e3af1ec61d04ff92f238b165dbc6d2a7b4ade7ed9812b4ce6b075e08f49fe" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
